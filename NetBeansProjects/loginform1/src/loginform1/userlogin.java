4026719aaca264b3666ad4d2a559de281a6f5ef0
guptaaditya978 <gupta.aditya978@gmail.com>
2019-06-11 15:26:43 +0530

    ku

--- NetBeansProjects/loginform1/src/loginform1/userlogin.java
+++ NetBeansProjects/loginform1/src/loginform1/userlogin.java
@@ -1,6 +1,7 @@
 package loginform1;
 
 import java.io.BufferedReader;
+import java.util.Base64;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,35 +15,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
-
+import java.sql.*;
 
 public class userlogin extends HttpServlet {
 	/**
 	 * 
 	 */
-	static ArrayList<mockusers> a1=new ArrayList<mockusers>(); 
+	 
 	
-	public void  init(ServletConfig config)throws ServletException
-	{
-		
-		mockusers b1=new mockusers(101,"A","1");
-		a1.add(b1);
-		mockusers b2=new mockusers(102,"B","12");
-		a1.add(b2);
-		mockusers b3=new mockusers(103,"C","123");
-		a1.add(b3);
-		mockusers b4=new mockusers(104,"D","1234");
-		a1.add(b4);
-		mockusers b5=new mockusers(105,"E","12345");
-		a1.add(b5);
-		mockusers b6=new mockusers(106,"F","123456");
-		a1.add(b6);
-	}
+//	public void  init(ServletConfig config)throws ServletException
+//	{
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/userlogin", "root", "root");
+//			Statement stmt = conn.createStatement();
+//			String sqlStr = "select * from users";  
+//			ResultSet rs = stmt.executeQuery(sqlStr); 
+//			int id;
+//			String str;
+//			String username;
+//			String password;
+//			while(rs.next()){
+//		         
+//		         id  = rs.getInt("id");
+//		         username = rs.getString("username");
+//		         str=rs.getString("password");
+//		         password= rs.getString("password");
+//		         System.out.println(id+username+password);
+//		         System.out.println();
+//		         }
+//			conn.close();
+//		}
+//		catch(Exception e)
+//		{
+//			System.out.println("database not connected");
+//		}
+//				
+//	}
 	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException 
-	{
+	{	
+		PrintWriter out=response.getWriter();
 		StringBuilder stringBuilder = new StringBuilder();
 	    BufferedReader bufferedReader = null;
-
 	    try {
 	        InputStream inputStream = request.getInputStream();
 	        if (inputStream != null) {
@@ -68,34 +82,40 @@ public class userlogin extends HttpServlet {
 	    }
 
 	    String body = stringBuilder.toString();
-	    mockusers usersData1 = new Gson().fromJson(body, mockusers.class);
-	    System.out.println(usersData1);
-	    if(usersData1==null) {
-	    	String n=new Gson().toJson(a1);
-	    	System.out.println(n);	    	
-	    }
-	    else {
-	    //System.out.println(body);
+//	    mockusers usersData1 = new Gson().fromJson(body, mockusers.class);
+//	    System.out.println(usersData1);
+//	    System.out.println(body);
 	    mockusers usersData = new mockusers(0, "","");
-	    PrintWriter out=response.getWriter();
-	    //System.out.println(request.getParameter("id").trim());
+	    System.out.println(request.getParameter("id").trim());
 	    usersData.id=Integer.parseInt(request.getParameter("id").trim());
 		if(usersData!=null)
 	    {
-		for(mockusers d : a1)
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/userlogin", "root", "root");
+				Statement stmt = conn.createStatement();
+				Base64.Decoder decoder = Base64.getDecoder(); 
+				String sqlStr = "select * from users where id='"+usersData.id+"'";  
+				ResultSet rs = stmt.executeQuery(sqlStr); 
+				byte[] actualByte;
+				while(rs.next()){
+			         
+			         usersData.id  = rs.getInt("id");
+			         usersData.name = rs.getString("username");
+			         actualByte= decoder.decode(rs.getString("password"));
+			         String dstr= new String(actualByte);;
+			         usersData.password=dstr;
+			         System.out.println("Fetched from database");
+			         String n=new Gson().toJson(usersData);
+			         out.println(n);
+			         System.out.println(n);
+				}
+				conn.close();
+			}
+			catch(Exception e)
 			{
-	    		if(d.getId()==usersData.id)
-		        {
-	    			usersData=d;
-	    			System.out.println("Fetched");
-	    			break;
-		        }
-		    }
-				String n=new Gson().toJson(usersData);
-				out.println(n);
-//				String z=new Gson().toJson(a1);
-//				out.println(z);
-	    }
+				System.out.println("database not connected");
+			}
 	    }
 	}
 	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException 
@@ -132,37 +152,61 @@ public class userlogin extends HttpServlet {
 	    //System.out.println(usersData);
 	    PrintWriter out=response.getWriter();
 	    //System.out.println(usersData.getName());
-	    int k=0;
+	    Connection conn= null;
+    	Statement stmt = null;
+    	
 	    if(usersData.getName().isEmpty()) 
 		{
-	    	//System.out.println();
-	    	for(mockusers d : a1)
-			{
-	    		if((d.getId() == usersData.getId()) && (d.getPassword().equals(usersData.getPassword())))
-		        {
-		        k=1;
-		        break;
-		        }
-		    }
-		    if(k==1)
-		    {
-				System.out.println("Successful");
-				String n=new Gson().toJson("Successfull");
-				out.print(n);	
+	    	
+	    	try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/userlogin", "root", "root");
+				stmt = conn.createStatement();
+				Base64.Encoder encoder = Base64.getEncoder();
+				String str= encoder.encodeToString(usersData.password.getBytes());  
+				String sqlStr = "Select * from users WHERE id='"+usersData.id+"' && password='"+str+"'";  
+				ResultSet rs = stmt.executeQuery(sqlStr);
+				//System.out.println(rs.getRow());
+				//System.out.println(rs.next());
+				//System.out.println(rs.getInt("id"));
+				if(rs.next())
+				{
+					System.out.println("Successful");
+					String n=new Gson().toJson("Successfull");
+					out.print(n);	
+				}
+				else {
+					System.out.println("Unsuccessfull");
+					String n=new Gson().toJson("Unsuccessfull");
+					out.print(n);	
+				}
+				conn.close();	
 			}
-			else
+			catch(Exception e)
 			{
-				System.out.println("Unsuccessfull");
-				String n=new Gson().toJson("Unsuccessfull");
-				out.print(n);
+				System.out.println("database not connected");
 			}
 		}
 		else
 		{
-		System.out.println("Added user");
-	    System.out.println(usersData);
-	    a1.add(usersData);
-//		if(request.getParameter("id")!=null) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/userlogin","root","root");
+			stmt = conn.createStatement();
+			Base64.Encoder encoder = Base64.getEncoder();
+			String str= encoder.encodeToString(usersData.password.getBytes());  
+			String sqlStr = "insert into users(id,username,password)values('"+usersData.id+"','"+usersData.name+"','"+str+"')";
+			stmt.executeUpdate(sqlStr);
+			System.out.println("Suscessfully added");
+			conn.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		
+	    //if(request.getParameter("id")!=null) {
 //		
 //		int id=Integer.parseInt(request.getParameter("id"));
 //		String name=request.getParameter("name");
@@ -172,8 +216,6 @@ public class userlogin extends HttpServlet {
 //		//JSONObject myJsonObject = new JSONObject();
 //		System.out.println(b.id);
 //		a1.add(b);
-		String n=new Gson().toJson(a1);
-		out.print(n);
 		}
 	}
 	
@@ -211,17 +253,22 @@ public class userlogin extends HttpServlet {
 	    usersData.id=Integer.parseInt(request.getParameter("id").trim());
 	    PrintWriter out=response.getWriter();
 		//JSONObject myJsonObject = new JSONObject();
-	    for(mockusers d : a1)
+	    try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/userlogin", "root", "root");
+			Statement stmt = conn.createStatement();
+			String sqlStr = "delete from users where id='"+usersData.id+"'";  
+			stmt.executeUpdate(sqlStr); 
+			System.out.println("Deleted Succesfully");
+			conn.close();
+		}
+		catch(Exception e)
 		{
-    		if(d.getId()==usersData.id)
-	        {
-    			a1.remove(d);
-    			break;
-	        }
-	    }
-		String n=new Gson().toJson("Deleted");
-		System.out.println(a1);
-		out.print(n);
+			System.out.println("database not connected");
+		}
+		//String n=new Gson().toJson("Deleted");
+		//System.out.println(a1);
+		//out.print(n);
 }
 	
 	public void doPut(HttpServletRequest request, HttpServletResponse response)throws IOException
@@ -259,21 +306,26 @@ public class userlogin extends HttpServlet {
 	    //int index=
 	    //if(a1.contains(usersData.id))
 	    	//int index=a1.indexOf(usersData.id);
-	    int i=0;
-	    for(mockusers d : a1)
-		{
-    		if(d.getId()==usersData.getId())
-	        {
-    			a1.set(i, usersData);
-    			System.out.println("Successfully Updated");
-    			break;
-	        }
-    		i++;
+	    try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/userlogin", "root", "root");
+			Statement stmt = conn.createStatement();
+			Base64.Encoder encoder = Base64.getEncoder();
+			String str= encoder.encodeToString((usersData.password).getBytes());  
+			System.out.println(str);
+			String sqlStr = "Update users set username='"+usersData.name+"', password='"+str+"'where id='"+usersData.id+"'";  
+			stmt.executeUpdate(sqlStr); 
+			System.out.println("Updated Succesfully");
+			PrintWriter out=response.getWriter();
+			String n=new Gson().toJson(usersData);
+			out.print(n);
+			conn.close();
 	    }
-	    System.out.println(a1);
-		PrintWriter out=response.getWriter();
-		String n=new Gson().toJson(usersData);
-		out.print(n);
+		catch(Exception e)
+		{
+			System.out.println("database not connected");
+		}
+	    
 //		}		
 	}
 	
